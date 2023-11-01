094291615d9a9f199fcd52b3646ff54e490b14d1
Semicome <wy2924107890@163.com>
2023-11-01 10:55:23 +0800

    11.1

---
+++ hw1/GuitarHero.java
@@ -0,0 +1,39 @@
+import synthesizer.GuitarString;
+
+public class GuitarHero {
+    private static final double CONCERT_A = 440.0;
+    private static final String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+    private static final int totalNotes = 37;
+
+    private static double setFrequency(int i) {
+        return CONCERT_A * Math.pow(2, (i - 24) / 12);
+    }
+
+    public static void main(String[] args) {
+        GuitarString[] gs = new GuitarString[totalNotes];
+        for (int i = 0; i < totalNotes; i++) {
+            gs[i] = new GuitarString(setFrequency(i));
+        }
+
+        while (true) {
+            if (StdDraw.hasNextKeyTyped()) {
+                char key = StdDraw.nextKeyTyped();
+                int index = keyboard.indexOf(key);
+                if (index < 0) {
+                    System.out.println("error!");
+                    continue;
+                }
+                gs[index].pluck();
+            }
+
+            double sample = 0;
+            for (synthesizer.GuitarString string : gs){
+                sample += string.sample();
+            }
+            StdAudio.play(sample);
+            for (synthesizer.GuitarString string : gs){
+                string.tic();
+            }
+        }
+    }
+}
--- hw1/synthesizer/GuitarString.java
+++ hw1/synthesizer/GuitarString.java
@@ -1,5 +1,7 @@
 // TODO: Make sure to make this class a part of the synthesizer package
-//package <package name>;
+package synthesizer;
+
+import synthesizer.BoundedQueue;
 
 //Make sure this class is public
 public class GuitarString {
@@ -18,6 +20,11 @@ public class GuitarString {
         //       cast the result of this divsion operation into an int. For better
         //       accuracy, use the Math.round() function before casting.
         //       Your buffer should be initially filled with zeros.
+        int capacity = (int)Math.round(SR / frequency);
+        buffer = new ArrayRingBuffer<Double>(capacity);
+        for (int i = 0; i < capacity; i++) {
+            buffer.enqueue(0.0);
+        }
     }
 
 
@@ -28,6 +35,13 @@ public class GuitarString {
         //       double r = Math.random() - 0.5;
         //
         //       Make sure that your random numbers are different from each other.
+        while (!buffer.isEmpty()) {
+            buffer.dequeue();
+        }
+        while (!buffer.isFull()) {
+            double r = Math.random() - 0.5;
+            buffer.enqueue(r);
+        }
     }
 
     /* Advance the simulation one time step by performing one iteration of
@@ -37,11 +51,13 @@ public class GuitarString {
         // TODO: Dequeue the front sample and enqueue a new sample that is
         //       the average of the two multiplied by the DECAY factor.
         //       Do not call StdAudio.play().
+        double x = buffer.dequeue();
+        buffer.enqueue(DECAY * (x + buffer.peek()) / 2);
     }
 
     /* Return the double at the front of the buffer. */
     public double sample() {
         // TODO: Return the correct thing.
-        return 0;
+        return buffer.peek();
     }
 }
